e9d7dfef6cb96e5995f3151780223a313a2ff3be
zhenggang <zhenggang@mofang.com>
2016-03-10 08:59:35 +0800

    first commit

---
+++ src/main/java/com/imwoniu/rabbit/rpc/RPCClient.java
@@ -0,0 +1,82 @@
+package com.imwoniu.rabbit.rpc;
+
+import com.rabbitmq.client.*;
+
+import java.util.UUID;
+
+/**
+ * Remote procedure call (RPC)
+ * Client
+ */
+public class RPCClient {
+    private Connection connection;
+    private Channel channel;
+    private String requestQueueName = "rpc_queue";
+    private String replyQueueName;
+    private QueueingConsumer consumer;
+
+    public RPCClient() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("123.57.242.222");
+        factory.setUsername("test");
+        factory.setPassword("test");
+
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+
+        replyQueueName = channel.queueDeclare().getQueue();
+        consumer = new QueueingConsumer(channel);
+
+        channel.basicConsume(replyQueueName, true, consumer);
+    }
+
+    public String call(String message) throws Exception {
+        String response = null;
+        String corrId = UUID.randomUUID().toString();
+        AMQP.BasicProperties props = new AMQP.BasicProperties()
+                .builder()
+                .correlationId(corrId)
+                .replyTo(replyQueueName)
+                .build();
+
+        channel.basicPublish("", requestQueueName, props, message.getBytes("UTF-8"));
+
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response = new String(delivery.getBody(), "UTF-8");
+                break;
+            }
+        }
+        return response;
+    }
+
+    public void close() throws Exception {
+        connection.close();
+    }
+
+    public static void main(String[] args) {
+        RPCClient rpcClient = null;
+        String response = null;
+        try {
+            rpcClient = new RPCClient();
+
+            System.out.println(" [x] Requesting fib(30)");
+
+            response = rpcClient.call("30");
+
+            System.out.println(" [.] Got '" + response + "'");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rpcClient != null) {
+                try {
+                    rpcClient.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/imwoniu/rabbit/rpc/RPCServer.java
@@ -0,0 +1,92 @@
+package com.imwoniu.rabbit.rpc;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+
+/**
+ * Remote procedure call (RPC)
+ * Server
+ */
+public class RPCServer {
+
+    private static final String RPC_QUEUE_NAME = "rpc_queue";
+
+    private static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        Channel channel = null;
+
+        try {
+
+            ConnectionFactory factory = new ConnectionFactory();
+
+            // 创建连接
+            factory.setHost("123.57.242.222");
+            factory.setUsername("test");
+            factory.setPassword("test");
+
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+
+            channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+
+            channel.basicQos(1);
+
+            QueueingConsumer consumer = new QueueingConsumer(channel);
+
+            channel.basicConsume(RPC_QUEUE_NAME, false, consumer);
+
+            System.out.println(" [x] Awaiting RPC requests");
+
+            while (true) {
+                String response = null;
+                QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+
+                BasicProperties props = delivery.getProperties();
+                AMQP.BasicProperties replyProps = new AMQP.BasicProperties()
+                        .builder()
+                        .correlationId(props.getCorrelationId())
+                        .build();
+
+                try {
+
+                    String message = new String(delivery.getBody(), "UTF-8");
+                    int n = Integer.parseInt(message);
+
+                    System.out.println(" [.] fib(" + message + ")");
+
+                    response = "" + fib(n);
+
+                } catch (Exception e) {
+                    System.out.println(" [.] " + e.toString());
+                    response = "";
+                } finally {
+                    channel.basicPublish("", props.getReplyTo(), replyProps, response.getBytes("UTF-8"));
+                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                }
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
